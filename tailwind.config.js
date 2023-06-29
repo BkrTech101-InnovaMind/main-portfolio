55860a47aefadf0e979a62e3876071bd4e1bb2a1
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-06-29 18:20:28 +0300

    refactor(tailwind.config): implement the default font for the website and the needed animation for the navbar icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        primary: "coding_font",
+      },
+      animation: {
+        "slide-down": "slide-down 0.5s ease-out",
+      },
+      keyframes: {
+        "slide-down": {
+          "0%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0)" },
+        },
       },
     },
   },
