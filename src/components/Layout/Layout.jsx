75c1d530c0a4182bc3273338d31b6210bca0562f
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-06-29 22:18:22 +0300

    refactor(layout): implement a numberd sidebar that will be shown in the hole pages

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react"
 import Navbar from "../Navbar"
 
 export default function Layout({ children }) {
+  const [numbers, setNumbers] = useState([])
+
+  useEffect(() => {
+    const handleResize = () => {
+      const windowWidth = window.innerWidth
+      setNumbers(
+        Array.from({ length: Math.floor(windowWidth / 60) }, (_, i) => i + 1)
+      )
+    }
+
+    window.addEventListener("resize", handleResize)
+    handleResize()
+
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
   return (
     <>
-      <Navbar />
-      {children}
+      <div className='flex h-screen'>
+        <aside className='border-r border-white h-screen px-4 text-lg'>
+          {numbers.map((number) => (
+            <div key={number}>{number}</div>
+          ))}
+        </aside>
+        <span className='flex-1 pr-10 pt-5'>
+          <Navbar />
+          <main className='mt-10'>{children}</main>
+        </span>
+      </div>
     </>
   )
 }
