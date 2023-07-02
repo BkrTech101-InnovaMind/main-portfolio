aa4dd4a95e4c07f6d0eaead2820ef694c590740a
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-07-02 04:37:31 +0300

    refactor(home page): implement the first image for the hero section

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
       <h2 className='text-center text-xl text-slate-200'>
         {"//"} HI, I&apos;M ABOBAKR, A...
       </h2>
-      <h1 className='text-3xl cursor-text text-[200%] md:text-[400%] lg:text-[700%] mt-8 border-2 flex justify-center py-3 md:py-6 lg:py-12'>
+      <h1 className='text-3xl text-[200%] md:text-[400%] lg:text-[700%] mt-8 flex justify-center'>
         <span className='text-slate-200'>{'{" '}</span>
         <Typewriter
           options={{
@@ -25,7 +25,11 @@ export default function HomePage() {
       <h2 className='inline-block rotate-90 text-3xl my-12'>
         <Link href='#'>{"==>"}</Link>
       </h2>
-      <div className='h-48 bg-hero-img bg-scroll'></div>
+      <div className='bg-hero-img bg-fixed'>
+        <div>
+          <h2 className='h2 py-36'></h2>
+        </div>
+      </div>
     </section>
   )
 }
