7f3ac6779757cef2f3adda4645f44e123a7ee949
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-07-06 00:13:49 +0300

    refactor(homepage): implementing the new section of the homepage which is the about me section

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,31 +4,36 @@ import Typewriter from "typewriter-effect"
 export default function HomePage() {
   return (
     <section className='mt-24 text-center'>
-      <h2 className='text-center text-xl text-slate-200'>
-        {"//"} HI, I&apos;M ABOBAKR, A...
-      </h2>
-      <h1 className='text-3xl text-[200%] md:text-[400%] lg:text-[700%] mt-8 flex justify-center'>
-        <span className='text-slate-200'>{'{" '}</span>
-        <Typewriter
-          options={{
-            strings: [
-              "_front-end developer",
-              "_mobile apk developer",
-              "_web designer",
-            ],
-            autoStart: true,
-            loop: true,
-          }}
-        />
-        <span className='text-slate-200'>{' "}'}</span>
-      </h1>
-      <h2 className='inline-block rotate-90 text-3xl my-12'>
-        <Link href='#'>{"==>"}</Link>
-      </h2>
-      <div className='bg-hero-img bg-fixed'>
-        <div>
-          <h2 className='h2 py-36'></h2>
+      <div className='hero_section'>
+        <h2 className='text-center text-xl text-slate-200'>
+          {"//"} HI, I&apos;M ABOBAKR, A...
+        </h2>
+        <h1 className='text-3xl text-[200%] md:text-[400%] lg:text-[700%] mt-8 flex justify-center'>
+          <span className='text-slate-200'>{'{" '}</span>
+          <Typewriter
+            options={{
+              strings: [
+                "_front-end developer",
+                "_mobile apk developer",
+                "_web designer",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+          <span className='text-slate-200'>{' "}'}</span>
+        </h1>
+        <h2 className='inline-block rotate-90 text-3xl my-12 text-slate-200'>
+          <Link href='#'>{"==>"}</Link>
+        </h2>
+        <div className='bg-hero-img-1 bg-fixed'>
+          <div>
+            <h2 className='h2 py-44'></h2>
+          </div>
         </div>
+      </div>
+      <div className='about_me'>
+        <h1 className='text-3xl text-[#9A99FF]'>{"{/}"}</h1>
       </div>
     </section>
   )
