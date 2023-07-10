a6241fd10752fcd5060f03f31341eaefcb5ac9fa
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-07-10 20:19:20 +0300

    refactor(navbar): implemented a navbar content file to short the navbar code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import navbar from "@/content/en/navbar"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import { useState } from "react"
@@ -5,29 +6,17 @@ import { AiOutlineClose } from "react-icons/ai"
 import { RxHamburgerMenu } from "react-icons/rx"
 
 export default function Navbar() {
-  const links = [
-    { logo: "BKR", href: "/" },
-    { name: "_home", href: "/" },
-    { name: "_about", href: "/about" },
-    { name: "_portfolio", href: "/portfolio" },
-    { name: "_services", href: "/services" },
-    { name: "_resume", href: "/resume" },
-    { name: "_contact", href: "/contact" },
-  ]
-
-  const logo = links.find((link) => link.logo)
   const router = useRouter()
   const [isOpen, setIsOpen] = useState(false)
+  const { logo, links } = navbar
 
   return (
     <nav className='text-2xl'>
       <div className='flex justify-between items-center pl-4'>
         {/* Logo */}
         <div className='text-[#f9f9f9]'>
-          {/* Name */}
-          <Link href={logo.href} className='flex gap-3'>
-            <h2>{"//"}</h2>
-            <h2>{logo.logo}</h2>
+          <Link href={logo.href}>
+            <h2>{logo.title}</h2>
           </Link>
           {/* Description */}
           <h3 className='text-end font-normal text-[#9897FC] leading-3'>
@@ -37,22 +26,20 @@ export default function Navbar() {
         {/* Large Screens */}
         <div className='hidden lg:block'>
           <ul className='flex gap-12'>
-            {links
-              .filter((link) => link.name)
-              .map((link) => (
-                <li key={link.name}>
-                  <Link
-                    className={`${
-                      router.pathname === link.href
-                        ? "text-[#2FCBEF]"
-                        : "md:hover:text-[#9594F5] transition"
-                    }`}
-                    href={link.href}
-                  >
-                    {link.name}
-                  </Link>
-                </li>
-              ))}
+            {links.map((link) => (
+              <li key={link.name}>
+                <Link
+                  className={`${
+                    router.pathname === link.href
+                      ? "text-[#2FCBEF]"
+                      : "md:hover:text-[#9594F5] transition"
+                  }`}
+                  href={link.href}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
         {/* Small Screens Icons*/}
@@ -73,22 +60,20 @@ export default function Navbar() {
       </div>
       {isOpen && (
         <ul className='animate-slide-down bg-[#212121] mt-5 fixed w-full z-10'>
-          {links
-            .filter((link) => link.name)
-            .map((link) => (
-              <li key={link.name} className='border-b border-gray-700'>
-                <Link
-                  className={`${
-                    router.pathname === link.href
-                      ? "text-[#2FCBEF] bg-[#2a2a2a]"
-                      : "md:hover:text-[#9594F5] transition"
-                  } block pt-2 h-12`}
-                  href={link.href}
-                >
-                  {link.name}
-                </Link>
-              </li>
-            ))}
+          {links.map((link) => (
+            <li key={link.name} className='border-b border-gray-700'>
+              <Link
+                className={`${
+                  router.pathname === link.href
+                    ? "text-[#2FCBEF] bg-[#2a2a2a]"
+                    : "md:hover:text-[#9594F5] transition"
+                } block pt-2 h-12`}
+                href={link.href}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       )}
     </nav>
